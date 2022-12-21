1ead7dea801f10b31884455b8ad45367ba9168f0
xeno <xeno@lv427.space>
2022-12-21 08:27:15 +0100

    solved part 2 - what a mess :/

--- Program.fs
+++ Program.fs
@@ -7,6 +7,25 @@ type Oper =
     | Mul of string * string
     | Add of string * string
     | Sub of string * string
+    | MulYou of string
+    | DivByYou of string
+    | DivYouBy of string
+    | AddYou of string
+    | MinusYou of string
+    | YouMinus of string
+    | MulYouC of int64
+    | DivCByYou of int64
+    | DivYouByC of int64
+    | AddYouC of int64
+    | CMinusYou of int64
+    | YouMinusC of int64
+    | MulC of string * int64
+    | AddC of string * int64
+    | CMinus of int64 * string
+    | MinusC of string * int64
+    | DivCBy of int64 * string
+    | DivByC of string * int64
+    | You
 
 type Monkey(name: string, oper: Oper) =
     member this.Name = name
@@ -34,6 +53,7 @@ input |> List.map (printfn "%A")
 
 type State(cache: Map<string, int64>, monkeys: Monkey list) =
     member this.Cache = cache
+    member this.Monkeys = monkeys
 
     member this.IsDone() = cache.ContainsKey "root"
 
@@ -44,6 +64,12 @@ type State(cache: Map<string, int64>, monkeys: Monkey list) =
             | Sub (m1, m2) when cache.ContainsKey m1 && cache.ContainsKey m2 -> Some(monkey.Name, cache[m1] - cache[m2])
             | Add (m1, m2) when cache.ContainsKey m1 && cache.ContainsKey m2 -> Some(monkey.Name, cache[m1] + cache[m2])
             | Mul (m1, m2) when cache.ContainsKey m1 && cache.ContainsKey m2 -> Some(monkey.Name, cache[m1] * cache[m2])
+            | AddC (m1, i) when cache.ContainsKey m1 -> Some(monkey.Name, cache[m1] + i)
+            | MulC (m1, i) when cache.ContainsKey m1 -> Some(monkey.Name, cache[m1] * i)
+            | MinusC (m1, i) when cache.ContainsKey m1 -> Some(monkey.Name, cache[m1] - i)
+            | CMinus (i, m1) when cache.ContainsKey m1 -> Some(monkey.Name, i - cache[m1])
+            | DivByC (m1, i) when cache.ContainsKey m1 -> Some(monkey.Name, cache[m1] / i)
+            | DivCBy (i, m1) when cache.ContainsKey m1 -> Some(monkey.Name, i / cache[m1])
             | _ -> None
 
         let adds =
@@ -52,8 +78,84 @@ type State(cache: Map<string, int64>, monkeys: Monkey list) =
         let rest = monkeys |> List.filter (fun m -> toAdd m |> Option.isNone)
         let full = [ cache |> Map.toSeq |> Seq.toList; adds ] |> List.concat |> Map.ofList
         State(full, rest)
-    member this.Root () =
-        cache.TryFind "root"
+
+    member this.EmbedYou() =
+        let embed (monkey: Monkey) =
+            match monkey.Oper with
+            | Mul (m1, "humn") -> Monkey(monkey.Name, MulYou m1)
+            | Mul ("humn", m1) -> Monkey(monkey.Name, MulYou m1)
+            | Div (m1, "humn") -> Monkey(monkey.Name, DivByYou m1)
+            | Div ("humn", m1) -> Monkey(monkey.Name, DivYouBy m1)
+            | Add (m1, "humn") -> Monkey(monkey.Name, AddYou m1)
+            | Add ("humn", m1) -> Monkey(monkey.Name, AddYou m1)
+            | Sub (m1, "humn") -> Monkey(monkey.Name, MinusYou m1)
+            | Sub ("humn", m1) -> Monkey(monkey.Name, YouMinus m1)
+            | MulYou m1 when cache.ContainsKey m1 -> Monkey(monkey.Name, MulYouC cache[m1])
+            | DivByYou m1 when cache.ContainsKey m1 -> Monkey(monkey.Name, DivCByYou cache[m1])
+            | DivYouBy m1 when cache.ContainsKey m1 -> Monkey(monkey.Name, DivYouByC cache[m1])
+            | AddYou m1 when cache.ContainsKey m1 -> Monkey(monkey.Name, AddYouC cache[m1])
+            | MinusYou m1 when cache.ContainsKey m1 -> Monkey(monkey.Name, CMinusYou cache[m1])
+            | YouMinus m1 when cache.ContainsKey m1 -> Monkey(monkey.Name, YouMinusC cache[m1])
+            | Add (m1, m2) when cache.ContainsKey m1 -> Monkey(monkey.Name, AddC(m2, cache[m1]))
+            | Add (m1, m2) when cache.ContainsKey m2 -> Monkey(monkey.Name, AddC(m1, cache[m2]))
+            | Mul (m1, m2) when cache.ContainsKey m1 -> Monkey(monkey.Name, MulC(m2, cache[m1]))
+            | Mul (m1, m2) when cache.ContainsKey m2 -> Monkey(monkey.Name, MulC(m1, cache[m2]))
+            | Sub (m1, m2) when cache.ContainsKey m1 -> Monkey(monkey.Name, CMinus(cache[m1], m2))
+            | Sub (m1, m2) when cache.ContainsKey m2 -> Monkey(monkey.Name, MinusC(m1, cache[m2]))
+            | Div (m1, m2) when cache.ContainsKey m1 -> Monkey(monkey.Name, DivCBy(cache[m1], m2))
+            | Div (m1, m2) when cache.ContainsKey m2 -> Monkey(monkey.Name, DivByC(m1, cache[m2]))
+            | _ -> monkey
+
+        let monkeys = monkeys |> List.map embed
+        State(cache, monkeys)
+
+    member this.SetValuesFromRoot() =
+        let root = monkeys |> List.find (fun monkey -> monkey.Name = "root")
+
+        match root.Oper with
+        | AddC (m1, c) ->
+            printfn $"* Setting {m1} to {c}"
+            let cache = cache.Add(m1, c)
+            // let monkeys = monkeys |> List.filter (fun m -> m.Name <> m1)
+            State(cache, monkeys)
+        | _ ->
+            printfn $"### Can't set values from {root}"
+            this
+
+    member this.RevStep() =
+        let revcache (monkey: Monkey) : Option<string * int64> =
+            if cache.ContainsKey monkey.Name then
+                printfn $"Cache has {monkey.Name}"
+                let value = cache[monkey.Name]
+
+                match monkey.Oper with
+                | AddC (m1, i) -> Some(m1, value - i)
+                | MulC (m1, i) when value <> 0 -> Some(m1, value / i)
+                | MinusC (m1, i) -> Some(m1, i + value)
+                | CMinus (i, m1) -> Some(m1, i - value)
+                | DivByC (m1, c) -> Some(m1, value * c)
+                | DivCBy (c, m1) when value <> 0 -> Some(m1, c / value)
+                | AddYouC c -> Some("YOU", value - c)
+                | YouMinusC c -> Some("YOU", value + c)
+                | CMinusYou c -> Some("YOU", c - value)
+                | MulYouC c when value <> 0 -> Some("YOU", value/c)
+                | DivYouByC c -> Some("YOU",value*c)
+                | DivCByYou c -> Some("YOU", c * value) 
+                | _ -> None
+            else
+                // printfn $"Cache is missing: {monkey.Name}"
+                None
+
+        let updates =
+            monkeys |> List.map revcache |> List.filter Option.isSome |> List.map Option.get
+
+        let cache =
+            [ cache |> Map.toSeq |> Seq.toList; updates ] |> List.concat |> Map.ofList
+        // printfn $"updates: {updates}"
+        State(cache, monkeys)
+    member this.You = cache.TryFind "YOU"
+
+    member this.Root() = cache.TryFind "root"
 
     static member init(monkeys: Monkey list) =
         let toNum (m: Monkey) : Option<string * int64> =
@@ -81,18 +183,61 @@ type State(cache: Map<string, int64>, monkeys: Monkey list) =
     override this.ToString() =
         $"Cache({cache.Count} {monkeys.Length} done={this.IsDone()})"
 
-let state = State.init input
-let state2 = state.Step()
-let state3 = state2.Step()
+let state = input |> State.init
 
-printfn $"{state}"
-printfn $"{state2}"
-printfn $"{state3}"
-
-let rec stepAll (state:State) =
-    if state.IsDone () then state
-    else stepAll (state.Step ())
+let rec stepAll (state: State) =
+    if state.IsDone() then state else stepAll (state.Step())
 
 let endState = stepAll state
 
-printfn $"{endState} {endState.Root ()}"
+printfn $"{endState} {endState.Root()}"
+
+let replaceWithYou (monkey: Monkey) =
+    if monkey.Name = "humn" then
+        Monkey(monkey.Name, You)
+    else
+        monkey
+
+let input2 = input |> List.map replaceWithYou
+
+let rec stepMax (state: State) =
+    let state2 = state.Step()
+
+    if state2.ToString() = state.ToString() then
+        state
+    else
+        stepMax state2
+
+let state2 = State.init input2 |> stepMax
+
+printfn $"state2 = {state2}"
+
+let rec embed (state: State) : State =
+    // state.Cache |> Map.toSeq |> Seq.toList |> List.map (printfn "%A")
+    // state.Monkeys |> List.map (printfn "%A")
+    printfn "STEP2: "
+    let state = state.EmbedYou()
+    let state = state.EmbedYou()
+    // state.Monkeys |> List.map (printfn "%A")
+    state
+
+let state3 = embed state2
+let state4 = state3.SetValuesFromRoot()
+printfn $"{state4}"
+
+let state5 = state4.RevStep()
+let state6 = state5.RevStep()
+
+printfn $"{state5} {state6}"
+
+let rec revSteps (state: State) =
+    let state2 = state.RevStep()
+
+    if state2.ToString() = state.ToString() then
+        state
+    else
+        revSteps state2
+
+let stateT2 = revSteps state4
+
+printfn $"FINAL {stateT2} {stateT2.You}"
