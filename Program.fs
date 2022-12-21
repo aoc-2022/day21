ebeaab4179efc142481ab7e448b3cb9348bb51ef
xeno <xeno@lv427.space>
2022-12-21 05:53:24 +0100

    made a new repo before todays tasks

---
+++ Program.fs
@@ -0,0 +1,8 @@
+﻿open System.IO
+
+let parse (s:string) =
+    match s.Split [|' '|] with
+    | s -> s
+let input = File.ReadAllLines "/tmp/aoc/input.t" |> Seq.map parse |> Seq.toList
+
+input |> List.map (printfn "%A")
